037f9a7282af7485bd44108ff6118e55ebb40390
poshekhon <shapehonwork@gmail.com>
2020-02-25 23:05:17 +0300

    Added Comments.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 # Регестрируем модель в админке
 admin.site.register(Post)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,9 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-# Модель поста
-
 
 class Post(models.Model):
+    """Модель Поста"""
     # Автор
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -25,6 +24,29 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def __str__(self):
         """Возвращает заголовок поста, если объект представляется в виде строки"""
         return self.title
+
+
+class Comment(models.Model):
+    """Модель Комментария"""
+    post = models.ForeignKey(
+        'blog.Post',
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
